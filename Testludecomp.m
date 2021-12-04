14139a3e06d25034228a29b06550c396de418919
comodt <95245113+comodt@users.noreply.github.com>
2021-12-03 20:29:13 -0700

    Update Testludecomp.m

--- Testludecomp.m
+++ Testludecomp.m
@@ -3,13 +3,13 @@
 %   -3*x1 - 6*x2 + 2*x3 = -61.5
 %   x1 + x2 + 5*x3 = -21.5
 % Set up A for the given system
-'''A = [10, 2, -1; 
+`A = [10, 2, -1; 
     -3, -6, 2;
-    1, 1, 5];'''
+    1, 1, 5];`
 % Set up b
-'b = [27; -61.5; -21.5];'
+`b = [27; -61.5; -21.5];`
 % Solve for x
-'x = A\b;'
+`x = A\b;`
 
 % Test your function
 '[L, U, P] = luFactor(A)'
